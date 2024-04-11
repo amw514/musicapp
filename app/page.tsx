d1ed3a535893a0750e0f9029baea22978ea21331
Ali Munir <alimunirwaris1@gmail.com>
2024-04-11 10:00:00 +0500

    working with featured section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]" >
       <HeroSection/>
+      <FeaturedSection/>
     </main>
   )}
