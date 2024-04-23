3ffc3cb9edabefc54407b778721270d2ab7956d6
Ali Munir <alimunirwaris1@gmail.com>
2024-04-23 19:51:48 +0500

    Add Footer component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedSection from "@/components/FeaturedSection";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
@@ -14,5 +15,6 @@ export default function Home() {
       <TestimonialCards/>
       <UpcomingWebinars/>
       <Instructors/>
+      <Footer/>
     </main>
   )}
